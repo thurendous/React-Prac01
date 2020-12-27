eeb8564ec4f73d9811ae9d670739e44989059088
thurendous <thurendous2@gmail.com>
2020-12-28 00:17:58 +0900

    daily grind

--- src/blog.jsx
+++ src/blog.jsx
@@ -1,18 +1,41 @@
 import React from "react";
 import Article from "./Article";
+// import {Foo, Bar} from "./components/FooBar";
+import * as FooBar from "./components/FooBar";
+import Hoge from "./components/Hoge";
 
 class Blog extends React.Component {
     constructor(props) {　// need this if it is class
         super(props);
         this.state = {
             isPublished: 1,
-            order: 1,
+            count: 0
+        }
+    }
+
+    componentDidMount() {
+        document.getElementById("counter").addEventListener("click", this.countUp)
+    }
+
+    componentDidUpdate() {
+        if (this.state.count > 1) {
+            this.setState({count: 0})
         }
     }
     // 公開状態を反転させる関数
     togglePublished  = () => {
+        // buttonクリックされたら、カウント数増える
         this.setState({ isPublished: !this.state.isPublished})
     }
+    countUp = () => {
+        this.setState({ count: this.state.count + 1})
+    }
+
+    // listenerの解除することでリソースを開放する　
+    componentWillUnmount() {
+        document.getElementById("counter").removeEventListener("click", this.countUp)
+    }
+
     render() {
         const authorName =  "ZZW";
         return (
@@ -22,7 +45,11 @@ class Blog extends React.Component {
                 title = "Reactの基本"
                 isPublished={this.state.isPublished}
                 toggle={()=>this.togglePublished()}
+                count={this.state.count}
                 />
+                <FooBar.Foo />
+                <FooBar.Bar />
+                <Hoge />
             </>
         )
     }
---
+++ src/components/FooBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+export function Foo() {
+    return(
+        <h2>Fooooooo</h2>
+    )
+};
+
+export function Bar() {
+    return(
+        <h2>Baaaaaaar</h2>
+    )
+};
+
