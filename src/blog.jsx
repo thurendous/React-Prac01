ccc09cf562236aeac866580f7acf8154419f16fa
thurendous <thurendous2@gmail.com>
2020-12-29 09:00:28 +0900

    daily grind

--- src/blog.jsx
+++ src/blog.jsx
@@ -4,55 +4,39 @@ import Article from "./Article";
 import * as FooBar from "./components/FooBar";
 import Hoge from "./components/Hoge";
 
-class Blog extends React.Component {
-    constructor(props) {　// need this if it is class
-        super(props);
-        this.state = {
-            isPublished: 1,
-            count: 0
-        }
-    }
+const Blog = () => {
+    // componentDidMount() {
+    //     document.getElementById("counter").addEventListener("click", this.countUp)
+    // }
 
-    componentDidMount() {
-        document.getElementById("counter").addEventListener("click", this.countUp)
-    }
+    // componentDidUpdate() {
+    //     if (this.state.count > 1) {
+    //         this.setState({count: 0})
+    //     }
+    // }
 
-    componentDidUpdate() {
-        if (this.state.count > 1) {
-            this.setState({count: 0})
-        }
-    }
-    // 公開状態を反転させる関数
-    togglePublished  = () => {
-        // buttonクリックされたら、カウント数増える
-        this.setState({ isPublished: !this.state.isPublished})
-    }
-    countUp = () => {
-        this.setState({ count: this.state.count + 1})
-    }
+    // countUp = () => {
+    //     this.setState({ count: this.state.count + 1})
+    // }
 
     // listenerの解除することでリソースを開放する　
-    componentWillUnmount() {
-        document.getElementById("counter").removeEventListener("click", this.countUp)
-    }
+    // componentWillUnmount() {
+    //     document.getElementById("counter").removeEventListener("click", this.countUp)
+    // }
 
-    render() {
         const authorName =  "ZZW";
         return (
             // can also be <React.fragment>
             <> 
                 <Article 
                 title = "Reactの基本"
-                isPublished={this.state.isPublished}
-                toggle={()=>this.togglePublished()}
-                count={this.state.count}
+                // count={this.state.count}
                 />
                 <FooBar.Foo />
                 <FooBar.Bar />
                 <Hoge />
             </>
         )
-    }
 }
 
 export default Blog;
